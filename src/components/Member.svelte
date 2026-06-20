f2a554e03b52fe017bfb8cf93709496e64b24d28
Madekuji <27665110+Madekuji@users.noreply.github.com>
2026-06-20 12:56:48 +0800

    add memberAvatar class and remove redundant socmed icon svg styling

--- src/components/Member.svelte
+++ src/components/Member.svelte
@@ -7,18 +7,18 @@
         src={
             "https://mineskin.eu/helm/"
             + (mc_username ? mc_username : "Alex")}
-        alt={ttv_username} width="72" height="72" />
+        class="memberAvatar" alt={ttv_username} width="72" height="72" />
     <div class="memberInfo">
         <span>{name ? name : ttv_username}</span>
         <div class="memberSocmed">
             {#if ttv_username}
                 <a href={"https://twitch.tv/" + ttv_username}>
-                    <img src="/ttv.svg" alt="Twitch" width="24" style="fill: purple;"/>
+                    <img src="/ttv.svg" alt="Twitch" width="24"/>
                     /{ttv_username}
                 </a>
             {:else if yt_username}
                 <a href={"https://youtube.com/@" + yt_username}>
-                    <img src="/yt.svg" alt="YouTube" height="24" style="fill: purple;"/>
+                    <img src="/yt.svg" alt="YouTube" height="24"/>
                     @{yt_username}
                 </a>
             {/if}
